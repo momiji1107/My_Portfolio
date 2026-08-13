ab3e29773b405310169b8db758271d7029263177
momiji1107 <yokohirahazuki0607@gmail.com>
2026-08-14 08:58:15 +0900

    vite.config.jsを修正し、ニュースセクションの「もっと見る」が正しく機能するようにした。

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  // GitHub Pages用のベースパスを設定
-  // GitHubリポジトリ名をかく
+
+  // GitHub Pages用のベースパス
   base: '/My_Portfolio/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        news: resolve(process.cwd(), 'news.html'),
+      },
+    },
+  },
 })
