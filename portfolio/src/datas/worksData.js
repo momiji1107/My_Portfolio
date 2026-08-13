efbddebacbee51247c11c43ddc657314190dccaa
momiji1107 <yokohirahazuki0607@gmail.com>
2026-08-14 01:12:41 +0900

    Worksセクションを実装した。
    作品データはworksData.jsで管理している。

---
+++ portfolio/src/datas/worksData.js
@@ -0,0 +1,53 @@
+export const worksData = [
+    {
+        title: 'スライムヒーロー',
+        period: '約5ヶ月',
+        members: '7人',
+        roles: 'プロジェクトリーダー / プログラミング / アップグレードシステム設計 / UI実装 ',
+        description: 'マップ探索やバトル、アイテム収集を楽しめる2DアクションRPGです。プレイヤーの成長やスキル習得によって、さまざまな戦略やプレイスタイルを体験できます。',
+        images: [
+            'img/works/slimeHero1.png',
+            'img/works/slime2.jpg',
+            'img/works/slime3.jpg',
+            'img/works/slime4.jpg',
+            'img/works/slime5.jpeg'
+        ],
+        github: 'https://github.com/momiji1107/Minge2026Spring_Team2'
+    },
+
+    {
+        title: '深隧 -Shinzui-',
+        period: '約6ヶ月',
+        members: '3人',
+        roles: 'プログラミング / 敵の行動処理実装 / ゲームシステム設計 / カメラ制御 / ステージデザイン',
+        description: 'ここにゲーム作品の説明文を記述します。制作時に工夫したポイントや、自分が担当した部分について紹介します。',
+        images: [
+            'img/works/slime2.jpg'
+        ],
+        github: 'https://github.com/PandD-Aizu/Minge2025Summer_Team3'
+    },
+
+    {
+        title: 'パズルナイツ',
+        period: '約6ヶ月',
+        members: '3人',
+        roles: 'プログラミング / ステージデザイン / UI設計',
+        description: 'ここにゲーム作品の説明文を記述します。制作時に工夫したポイントや、自分が担当した部分について紹介します。',
+        images: [
+            'img/works/slime2.jpg'
+        ],
+        github: 'https://github.com/PandD-Aizu/Minge2025Spring_Team1'
+    },
+
+    {
+        title: 'Lost of Music',
+        period: '約4ヶ月',
+        members: '4人',
+        roles: 'プログラミング / ギミック考案 / ステージデザイン / UI設計',
+        description: 'ここにゲーム作品の説明文を記述します。ゲームのジャンルや特徴、プレイヤーがどのような体験を楽しめる作品なのかを紹介します。',
+        images: [
+            'img/works/slime2.jpg'
+        ],
+        github: 'https://github.com/PandD-Aizu/Minge2024Summer_Team5'
+    }
+];
